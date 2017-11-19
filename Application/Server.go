86e69ef07cff207d39c5d47ab528e09cf4843438
ccaustinT <austin@crosschannel.com>
2017-11-18 17:45:17 -0800

    Updated formating

--- Application/Server.go
+++ Application/Server.go
@@ -1,24 +1,25 @@
 package main
 
-import "fmt"
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-ticker := time.NewTicker(5 * time.Second)
-quit := make(chan struct{})
-    for {
-       select {
-        case <- ticker.C:
-            tick()
-        case <- quit:
-            ticker.Stop()
-            return
-        }
-    }
+	ticker := time.NewTicker(5 * time.Second)
+	quit := make(chan struct{})
+	for {
+		select {
+		case <-ticker.C:
+			tick()
+		case <-quit:
+			ticker.Stop()
+			return
+		}
+	}
 }
 
 //TODO: AT - Add calculator function that adds to number
 func tick() {
 	fmt.Println("Hello World")
 }
-
